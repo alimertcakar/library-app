38de2fd4f5a7adb66bec0464fbea947e0c904877
Ali Mert <mail@alimert.net>
2020-03-05 00:58:46 +0300

    Inıt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './style/reset.css';
+import './style/skeleton.css';
+import './style/typography.css';
 
+import Search from './components/Search';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Library App
+
+      <div className="two columns offset-by-five">
+        <Search />
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/components/Search.css';
+
+
+
+export default function Search() {
+    return (
+        <div className="Search">
+            <input type="text" id="Search"></input>
+        </div>
+    );
+}
